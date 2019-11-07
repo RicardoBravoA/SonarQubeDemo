e70b1d347fd91721304a263f247b5cc06ee7b57e
Ricardo Bravo <ricardobravo@outlook.com>
2019-11-07 11:14:35 -0500

    chore: initial commit

---
+++ app/src/main/java/com/rba/sonarqubedemo/CodeSmell.kt
@@ -0,0 +1,25 @@
+package com.rba.sonarqubedemo
+
+class CodeSmell {
+
+    fun smell_1() {
+        if(1>0){
+            if(1>0){
+                if(1>0){
+                    if(1>0){
+                        if(1>0){
+                            if(1>0){
+                                if(1>0){
+                                    if(1>0){
+                                        print("1>0")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
